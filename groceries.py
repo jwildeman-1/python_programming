ee2a1f81002282b5bfc14d881db827598be275b8
Jason Wildeman <jw5236@nyu.edu>
2017-07-10 20:19:50 -0400

    Sort prodcuts by name

--- groceries.py
+++ groceries.py
@@ -1,4 +1,5 @@
 import code
+import operator
 
 products = [
     {"id":1, "name": "Chocolate Sandwich Cookies", "department": "snacks", "aisle": "cookies cakes", "price": 3.50},
@@ -25,6 +26,8 @@ products = [
 
 print("--------------")
 print("THERE ARE", (len(products)), "PRODUCTS:")
+
+products = sorted(products, key=operator.itemgetter("name"))
 
 for product in products:
     print(" + " + product["name"])
